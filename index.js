efdc03fe06aa2ab21e450810cef2d0953de09b45
向山　翔 <hms0175km@gmail.com>
2022-01-08 18:34:50 +0900

    [Finish]複製とカウントアップ

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 Vue.component('my-component', {
     data: function(){
         return {
-            number: 12
+            number: 0
         }
     },
-    template:'<p>いいね({{number}})</p>'
+    template: '<p>いいね({{number}})<button @click="increment">+1</button></p>',
+    methods: {
+        increment: function () {
+            this.number += 1;
+        }
+    },
 })
 
 new Vue({
     el: '#app',
 })
+
+// var data = {
+//     number: 0
+// }
+
+// Vue.component('my-component', {
+//     data: function(){
+//         return data
+//     },
+//     template: '<p>いいね({{number}})<button @click="increment">+1</button></p>',
+//     methods: {
+//         increment: function () {
+//             this.number += 1;
+//         }
+//     },
+// })
+
+// new Vue({
+//     el: '#app',
+// })
